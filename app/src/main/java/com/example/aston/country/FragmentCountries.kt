de46c3892aa3214235084eb7aa65fd787430b19e
Gabrusevich_V_V <tyson.cat1706@gmail.com>
2023-10-03 00:14:03 +0300

    Recyclerview

---
+++ app/src/main/java/com/example/aston/country/FragmentCountries.kt
@@ -0,0 +1,44 @@
+package com.example.aston.country
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.aston.MainActivity
+import com.example.aston.databinding.FragmentCountriesBinding
+
+class FragmentCountries : Fragment() {
+    private var _binding: FragmentCountriesBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var adapter: CountryAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCountriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as MainActivity).makeVisibility(false)
+    }
+    
+    private fun initViews() {
+        val manager = LinearLayoutManager(requireContext())
+        adapter = CountryAdapter()
+        binding.recyclerView.layoutManager = manager
+        binding.recyclerView.adapter = adapter
+        val items = CountryRepository()
+        adapter.setItems(items.countries)
+    }
+}
